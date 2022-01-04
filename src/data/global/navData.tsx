2f64bc80679e452e131b23ee4bd6f7f74c1c2642
folathecoder <officialakinloyefolarin@gmail.com>
2022-01-04 11:07:33 +0100

    Menu Data ==== Created

---
+++ src/data/global/navData.tsx
@@ -0,0 +1,38 @@
+const navData = {
+  logo: {
+    image: "",
+    altText: "loopstudios",
+    link: "/",
+  },
+  menu: [
+    {
+      title: "about",
+      link: "/",
+    },
+    {
+      title: "careers",
+      link: "/",
+    },
+    {
+      title: "events",
+      link: "/",
+    },
+    {
+      title: "products",
+      link: "/",
+    },
+    {
+      title: "support",
+      link: "/",
+    },
+  ],
+  social: [
+    { title: "facebook", icon: "/", link: "https://facebook.com" },
+    { title: "twitter", icon: "/", link: "https://twitter.com" },
+    { title: "pinterest", icon: "/", link: "https://pinterest.com" },
+    { title: "instagram", icon: "/", link: "https://instagram.com" },
+  ],
+  copyright: "© 2021 Loopstudios. All rights reserved.",
+};
+
+export default navData;
