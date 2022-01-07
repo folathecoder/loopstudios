f5c269abac1a221d7943c45ecb87c3ac73efcded
folathecoder <officialakinloyefolarin@gmail.com>
2022-01-07 07:02:46 +0100

    Theme ==== Custom MUI Theme Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
 import { FC } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { createTheme, ThemeProvider } from "@mui/system";
+import { CssBaseline } from "@mui/material";
 //TODO: Import Pages (Routes)
 import Homepage from "routes/Homepage";
 
+declare module "@mui/material/styles/createPalette" {
+  interface CommonColors {
+    layout: {
+      offBlack: string;
+      offWhite: string;
+    };
+  }
+}
+
+
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3221ce",
+    },
+  },
+});
+
 const App: FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
--- src/routes/Homepage.tsx
+++ src/routes/Homepage.tsx
@@ -1,6 +1,14 @@
 import { FC } from "react";
 // import { Helmet } from "react-helmet";
 
+import { styled } from "@mui/styles";
+
+const Header = styled("header")(({ theme }) => ({
+  minHeight: "3rem",
+  maxWidth: "100%",
+  backgroundColor: theme.palette.primary.main,
+}));
+
 const Homepage: FC = () => {
   return (
     <>
@@ -9,6 +17,7 @@ const Homepage: FC = () => {
         <title>Hello from React Helmet</title>
         <meta name="description" content="Basic example" />
       </Helmet> */}
+      <Header></Header>
       <div>
         <h1>Homepage is working!</h1>
       </div>
